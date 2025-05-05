7538015e67433ae727fcf2b4372b9df7fc45537e
chaimae0000 <aitchelhchaimae@gmail.com>
2025-05-05 02:49:16 +0100

    ajout du page des evenements+formulaire d'ajout

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;  
 use App\Http\Controllers\Admin\EventController;
 use App\Http\Controllers\Admin\AdherantController;
+use Illuminate\Support\Str;
 Route::resource('events', EventController::class);
 
 
@@ -19,4 +20,7 @@ Route::middleware(['auth', 'admin'])->group(function () {
 
 Route::get('/admin/gestion_adherants/visualiser-adherants', [AdherantController::class, 'index'])->name('gestion_adherants.visualiser_adherants');
 
+Route::get('/admin/dashboard/events/list', [EventController::class, 'index'])->name('events.list');
+Route::get('/admin/setting', [EventController::class, 'index'])->name('setting');
+Route::get('/admin/dashboard/events/list/show', [EventController::class, 'index'])->name('events.show');
 
